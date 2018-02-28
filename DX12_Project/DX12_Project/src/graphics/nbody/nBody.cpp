4f1e110b29010c6c4b7b930dd1824c2f6fbd0a60
Kimpai <kenarsson@hotmail.se>
2018-02-28 10:34:50 +0100

    changed mass value in compute shader back to correct value

--- DX12_Project/DX12_Project/src/graphics/nbody/nBody.cpp
+++ DX12_Project/DX12_Project/src/graphics/nbody/nBody.cpp
@@ -67,7 +67,7 @@ namespace dx
 	{
 		//Update the data for the compute constant buffer
 		CB_UPDATE cbUpdate;
-		cbUpdate.g_timestep = 0.016f;
+		cbUpdate.g_timestep = 0.0016f;
 		cbUpdate.g_softeningSquared = 0.0012500000f * 0.0012500000f;
 		cbUpdate.g_numParticles = NUM_BODIES;
 		m_buffer->SetConstantBufferData(&cbUpdate, sizeof(cbUpdate), frameIndex, &m_cbUpdateAddress[0]);
